acf2b2f87af187fb02f28b178618ddd7c7fc1aa2
Nathan-J. Hirschauer <nathanhi@deepserve.info>
2015-03-15 18:41:54 +0000

    Added comments; Fixed Windows build

--- src/tarantula.c
+++ src/tarantula.c
@@ -14,37 +14,49 @@
 #endif
 
 tar __rawToNorm(struct tar_raw *header) {
+	/* Converts a RAW tar header (all char,
+	 * no ints) to a normalized header (chars+ints)
+	 */
 	struct tar norm_header;
+
+	/* Keep these values as chars */
 	strncpy(norm_header.filename, header->filename, 100);
+	strncpy(norm_header.checksum, header->checksum, 8);
+	strncpy(norm_header.linktarget, header->linktarget, 100);
+	strncpy(norm_header.ustarindicator, header->ustarindicator, 6);
+	strncpy(norm_header.owner_username, header->owner_username, 32);
+	strncpy(norm_header.owner_groupname, header->owner_groupname, 32);
+	strncpy(norm_header.filename_prefix, header->filename_prefix, 155);
+	
+	/* Convert these valuese to int */
 	norm_header.filemode = atoi(header->filemode);
 	norm_header.owner_UID = atoi(header->owner_UID);
 	norm_header.owner_GID = atoi(header->owner_GID);
 	norm_header.filesize = atoi(header->filesize);
 	norm_header.modification_time = atoi(header->modification_time);
-	strncpy(norm_header.checksum, header->checksum, 8);
 	norm_header.typeflag = atoi(header->typeflag);
-	strncpy(norm_header.linktarget, header->linktarget, 100);
-	strncpy(norm_header.ustarindicator, header->ustarindicator, 6);
 	norm_header.ustarversion = atoi(header->ustarversion);
-	strncpy(norm_header.owner_username, header->owner_username, 32);
-	strncpy(norm_header.owner_groupname, header->owner_groupname, 32);
 	norm_header.device_majornumber = atoi(header->device_majornumber);
 	norm_header.device_minornumber = atoi(header->device_minornumber);
-	strncpy(norm_header.filename_prefix, header->filename_prefix, 155);
+
 	return norm_header;
 }
 
-tar getHeader(const char *tarfile)
-{
+tar getHeader(int offset, const char *tarfile) {
+	struct tar_raw header;
+	struct tar header_norm;
+
 #ifdef POSIX
 	char *f;
 	struct stat s;
-	struct tar_raw header;
-	struct tar header_norm;
 	int fd = open(tarfile, O_RDONLY);
+	int curr_offset = 0;
 
 	if ((fd >= 0) && (fstat(fd, &s) == 0)) {
+		/* If open and fstat worked */
 		f = (char *) mmap (0, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+		/* Map fields from header to respective variables in struct */
 		memcpy(header.filename, f+FILENAME_OFFSET, sizeof(header.filename));
 		memcpy(header.filemode, f+FILEMODE_OFFSET, sizeof(header.filemode));
 		memcpy(header.owner_UID, f+UID_OFFSET, sizeof(header.owner_UID));
@@ -61,11 +73,14 @@ tar getHeader(const char *tarfile)
 		memcpy(header.device_majornumber, f+DEVMAJORNUM_OFFSET, sizeof(header.device_majornumber));
 		memcpy(header.device_minornumber, f+DEVMINORNUM_OFFSET, sizeof(header.device_minornumber));
 		memcpy(header.filename_prefix, f+FILENAMEPREFIX_OFFSET, sizeof(header.filename_prefix));
+
+		/* Convert from raw to normalized header struct */
 		header_norm = __rawToNorm(&header);
+
 		char data[header_norm.filesize];
 		memcpy(data, f+HEADERLEN, sizeof(data));
 		printf("data: %s\n", data);
 	}
-	return header_norm;
 #endif
+	return header_norm;
 }
